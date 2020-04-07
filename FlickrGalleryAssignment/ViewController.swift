66ed8c1fec1d38a4f6d8b5ee6871395c376b3c43
Donovan Söderlund <donovan@futurememories.se>
2020-04-07 09:47:07 +0200

    Initial Commit

---
+++ FlickrGalleryAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrGalleryAssignment
+//
+//  Created by Donovan Söderlund on 2020-04-07.
+//  Copyright © 2020 Future Memories. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
